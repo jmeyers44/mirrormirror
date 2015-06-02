cd37a61fd201b425203b223bf695b000410fe061
hidace <dacewillmott@gmail.com>
2015-06-02 17:57:50 -0400

    format users show page list of songs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @songs = Song.all
-    # @artist = Song.find_by(artist_id)
-    # @album = Song.find_by(album_id)
+    @users = User.all
   end
 
 end
