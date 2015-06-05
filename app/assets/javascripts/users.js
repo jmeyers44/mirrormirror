70b281ca0463a9f17c3430bc5fade780700e015b
hidace <dacewillmott@gmail.com>
2015-06-05 11:09:31 -0400

    lock nav for scroll

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,35 +1,13 @@
-// $(document).ready(function(){
-//     $('.play').click(function(){
-//       $('#player').removeClass('hide').addClass('show');
-      // $('.card').removeClass('card video');
-    // });
-    // $('#player').data('size','big');
+$(document).ready(function(){
+  // $('.play').on('click', function(){
+      // debugger;
+    // $('.responsive-object').on("load", ".play", function(){
+    //   debugger;
+    // })
+  
+  $('.play').on('click', function(){
 
-    //   $(window).scroll(function(){
-    //     if($(document).scrollTop() > 0)
-    //     {
-
-    //       if($('#player').data('size') == 'big')
-    //       {
-    //           $('#player').data('size','small');
-    //           $('#player').stop().animate({
-    //               height:'150px'
-    //           },600);
-    //       }
-    //     }
-
-      // else
-
-      //   {
-      //     if($('#player').data('size') == 'small')
-      //     {
-      //         $('#player').data('size','big');
-      //         $('#player').stop().animate({
-      //             height:'100px'
-      //         },600);
-      //     }  
-      //   }
-      // });
-
-// });
+    
+  })
+});
 
