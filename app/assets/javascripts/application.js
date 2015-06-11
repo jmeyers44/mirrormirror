5e0ceccb23e2ecc47697746fc530591850e57e28
Jonathan Meyers <jmeyers44@gmail.com>
2015-06-11 15:00:34 -0400

    debug big video issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery.embedly-3.1.1.min.js
-//= require bigvideo
 //= require_tree .
