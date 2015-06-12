90e7cfcc91473ed2ec385fec282a7e1f42da2ff3
Jonathan Meyers <jmeyers44@gmail.com>
2015-06-12 09:55:12 -0400

    extend timeout window

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,4 +1,6 @@
 if Rails.env ==  "production"
-Rack::Timeout.timeout = 600
 Rack::Timeout.wait_timeout = 600
+Rack::Timeout.timeout = 600
+Rack::Timeout.service_past_wait = true 
+Rack::Timeout.wait_overtime = 600
 end
