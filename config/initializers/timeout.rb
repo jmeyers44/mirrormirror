c274f4fd87637cc69cb53bd81e18c817afc66127
Jonathan Meyers <jmeyers44@gmail.com>
2015-06-12 10:05:39 -0400

    add rack service timout

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,6 +1,5 @@
 if Rails.env ==  "production"
+Rack::Timeout.service_timeout = 600
 Rack::Timeout.wait_timeout = 600
-Rack::Timeout.timeout = 600
-Rack::Timeout.service_past_wait = true 
-Rack::Timeout.wait_overtime = 600
+Rack::Timeout.timeout = 600 
 end
